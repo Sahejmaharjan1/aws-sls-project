66b93ee410d37955bf3290796f1c930caa0d098c
Sahej Maharjan <sahej@bottle.com.np>
2022-03-17 10:39:23 +0545

    stepfunction

---
+++ functions/stepFunctionLambda/addSignup.js
@@ -0,0 +1,14 @@
+const Responses = require('../common/API_Responses')
+const Dynamo = require('../common/Dynamo')
+const { v4: uuid } = require('uuid')
+// import { v4 as uuid } from 'uuid'
+
+const tableName = process.env.signupTableName
+
+exports.handler = async (event) => {
+    console.log('event', event)
+    const email = event.Input.signup.email;
+    const ID = uuid()
+    await Dynamo.write({ email, ID, played: 'false' }, tableName);
+    return { ID }
+}
---
+++ functions/stepFunctionLambda/getSignup.js
@@ -0,0 +1,10 @@
+const Dynamo = require('../common/Dynamo')
+
+const tableName = process.env.signupTableName
+
+exports.handler = async (event) => {
+    console.log('event', event)
+    const ID = event.Input.Payload.ID;
+    const row = await Dynamo.get(ID, tableName);
+    return row
+}
